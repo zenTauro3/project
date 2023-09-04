d0316fd5481afa9576efec8f1ed43a85492836b0
zenTauro3 <jaumeizqbroch@gmail.com>
2023-09-04 02:53:16 +0200

    project

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import User from "../models/user";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 async function auth(request: Request, response: Response) {
     try {
@@ -17,35 +18,55 @@ async function auth(request: Request, response: Response) {
 async function register(request: Request, response: Response) {
     try {
         const { username, email, password } = request.body;
-        const validateUsername = await User.findOne({ username });
-        const validateEmail = await User.findOne({ email });
-        console.log(validateEmail)
-        if (!validateEmail && !validateUsername) {
-            const userInfo = new User({ username, email, password });
-            await userInfo.save();
-            return response.status(200).send("Registered successfully")
-        } else {
-            return response.status(400).send("Username or email already exists")
+
+        //validate inputs
+        const validateUsername = /^[a-zA-Z0-9_]{8,20}$/.test(username);
+        const validateEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+        const validatePassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,50}$/.test(password);
+
+        if (!validateUsername || !validateEmail || !validatePassword) {
+            return response.status(500).send("Invalid credentials")
         }
+
+        //validate database
+        const usernameExists = await User.findOne({ username });
+        const emailExists = await User.findOne({ email });
+
+        if (usernameExists || emailExists) {
+            return response.status(400).send("Username or email already exists");
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+        const userInfo = new User({ username, email, password: hashed });
+        await userInfo.save();
+        return response.status(200).send("Registered successfully")
     } catch {
-        return response.status(400).send("Error registering")
+        return response.status(500).send("Error registering")
     }
 }
 
 async function login(request: Request, response: Response) {
     try {
         const { email, password } = request.body;
-        const userInfo = await User.findOne({ email });
-        console.log(userInfo?.password === password)
-        if (userInfo && userInfo.password === password) {
-            const data = { username: userInfo.username, email: userInfo.email }
-            const token = jwt.sign(data, process.env.JWT || "", { expiresIn: '2h' });
-            return response.status(200).send(token);
-        } else {
-            return response.status(400).send("Incorrect email or password")
+
+        //validate inputs
+        if (!email || !password) {
+            return response.status(500).send("Invalid credentials")
         }
+
+        //validate database
+        const userInfo = await User.findOne({ email });
+        const isValid = await bcrypt.compare(password, userInfo?.password || "");
+
+        if (!userInfo || !isValid) {
+            return response.status(400).send("Incorrect email or password");
+        }
+
+        const data = { username: userInfo.username, email: userInfo.email }
+        const token = jwt.sign(data, process.env.JWT || "", { expiresIn: '2h' });
+        return response.status(200).send(token);
     } catch {
-        return response.status(400).send("Incorrect email or password")
+        return response.status(500).send("Error loging in")
     }
 }
 

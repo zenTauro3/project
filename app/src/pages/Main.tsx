fcc3c5e5824581901a20e9973c327c68b738ae9c
zenTauro3 <jaumeizqbroch@gmail.com>
2023-09-10 00:49:44 +0200

    project

--- app/src/pages/Main.tsx
+++ app/src/pages/Main.tsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Main() {
     return (
-        <>
-            <h1>Main</h1>
-            <Link to="/auth/login">Log in</Link>
-            <Link to="/auth/register">Register</Link>
-        </>
-    )
-};
+        <div className="min-h-screen bg-gradient-to-b from-gray-200 to-gray-300">
+            <header className="bg-blue-500 p-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <Link to="/" className="text-white text-2xl font-semibold">My App</Link>
+                    <div className="space-x-4">
+                        <Link to="/auth/login" className="text-white">Log in</Link>
+                        <Link to="/auth/register" className="text-white">Register</Link>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
 
-export default Main
+export default Main;
